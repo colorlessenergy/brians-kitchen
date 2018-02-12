166cec4a1666868ee69b2b9d260b3e401c7e0ccd
colorlessenergy <brianmunozdev@gmail.com>
2018-02-12 13:42:39 -0800

    add a working video and link to other sites

--- _site/main.js
+++ _site/main.js
@@ -22,17 +22,57 @@ $( document ).ready(function() {
 
   // modal for youtube video
 
+
+
+
   $('.hero-text').each(function (current) {
     $(this).on('click', function () {
       var currentM = document.querySelectorAll('.modal');
-      currentM[current-2].classList.toggle("show")
-    })
+      currentM[current-2].classList.toggle("show");
+    });
 
     $('.close-modal').each(function (current) {
       $(this).on('click', function () {
-        var currentM = document.querySelectorAll('.modal');
-        currentM[current].classList.toggle("show")
+          var currentM = document.querySelectorAll('.modal');
+          currentM[current].classList.toggle("show");
+
       })
     })
   });
+
 });
+
+// for youtube players
+
+var player = document.querySelectorAll(".player");
+var allPlayers = [];
+  // this function gets called when API is ready to use
+  function onYouTubePlayerAPIReady() {
+    player.forEach(function (current, index) {
+      var currentPlayer =  new YT.Player(current.id, {
+        events: {
+          // call this function when player is ready to use
+          'onReady': onPlayerReady
+        }
+      });
+      allPlayers.push(currentPlayer)
+
+    })
+  }
+
+  function onPlayerReady(event) {
+    console.log(player);
+    var pauseButton = document.querySelectorAll(".close-modal");
+    pauseButton.forEach(function (current, index) {
+      current.addEventListener("click", function() {
+        allPlayers[index].pauseVideo();
+
+      });
+    });
+  }
+
+  // Inject YouTube API script
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
