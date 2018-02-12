91dbbe9b66ec380d9d718e8671af24db898a3fbe
colorlessenergy <brianmunozdev@gmail.com>
2018-02-12 11:17:15 -0800

    add iframe for video player

--- _site/main.js
+++ _site/main.js
@@ -1,21 +1,38 @@
-var iconHamburger = document.querySelector("#icon-hamburger");
+$( document ).ready(function() {
+  // setting for the responsive nav and enable the responsive navigation
 
-var showReponsiveNav = document.querySelector(".responsive-nav");
+  var iconHamburger = document.querySelector("#icon-hamburger");
+  var showReponsiveNav = document.querySelector(".responsive-nav");
+  iconHamburger.addEventListener("click", function () {
+      showReponsiveNav.classList.toggle("responsive-nav-show")
+  });
 
-iconHamburger.addEventListener("click", function () {
+  // owl carousel
 
-    showReponsiveNav.classList.toggle("responsive-nav-show")
+  $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      items: 1,
+      mouseDrag: false,
+      navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+      touchDrag: false,
+  })
+
+
+  // modal for youtube video
+
+  $('.hero-text').each(function (current) {
+    $(this).on('click', function () {
+      var currentM = document.querySelectorAll('.modal');
+      currentM[current-2].classList.toggle("show")
+    })
+
+    $('.close-modal').each(function (current) {
+      $(this).on('click', function () {
+        var currentM = document.querySelectorAll('.modal');
+        currentM[current].classList.toggle("show")
+      })
+    })
+  });
 });
-
-
-// owl carousel
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    items: 1,
-    mouseDrag: false,
-    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    touchDrag: false,
-})
